5afc69b135f6550708ca60cbafcf55dbc111a523
nicocabrerafit2 <nicocabrera8@outlook.com>
2022-10-20 23:30:19 -0300

    Avances en mercado liebre

--- app.js
+++ app.js
@@ -3,8 +3,17 @@ const app = express ();
 const path = require('path');
 
 app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname + '/home.html')
+    res.sendFile(path.join(__dirname + '/views/home.html')
 )})
+
+app.get('/login', (req,res) => {
+    res.sendFile(path.join(__dirname + '/views/login.html')
+)})
+
+app.get('/register', (req,res) => {
+    res.sendFile(path.join(__dirname + '/views/register.html')
+)})
+
 
 app.listen(3000, () =>{
     console.log('Esta corriendo por puerto',3000);
