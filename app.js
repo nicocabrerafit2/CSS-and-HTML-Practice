5c7e7eca07000092438faecdb2962956e4739f1e
nicocabrerafit2 <nicocabrera8@outlook.com>
2022-11-03 17:28:41 -0300

    incorporo animaciones

--- app.js
+++ app.js
@@ -1,25 +1,21 @@
 const express = require("express");
-const app = express ();
-const path = require('path');
+const app = express();
+const path = require("path");
 
-app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname + '/views/home.html')
-)})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/home.html"));
+});
 
-app.get('/login', (req,res) => {
-    res.sendFile(path.join(__dirname + '/views/login.html')
-)})
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/login.html"));
+});
 
-app.get('/register', (req,res) => {
-    res.sendFile(path.join(__dirname + '/views/register.html')
-)})
-
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/register.html"));
+});
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log('Servidor corriendo en el puerto ${port}'));
+app.listen(port, () => console.log("Servidor corriendo en el puerto ${port}"));
 
-
-app.use(express.static(__dirname+ "/Public"))
-
-
+app.use(express.static(__dirname + "/Public"));
